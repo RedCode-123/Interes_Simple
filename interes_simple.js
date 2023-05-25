eb1ff531bc82fb93df38727fda8c5158e0d4f793
RedCode-123 <erar123@gmail.com>
2023-05-24 19:30:10 -0600

    Cambios antes del 24 de mayo del 2023

--- interes_simple.js
+++ interes_simple.js
@@ -9,53 +9,53 @@ function btnAction() {
         tiempo = document.querySelector("#tiempo").value,
         result = document.querySelector(".result"),
         values = validateData(interes, capital, rendimiento, tiempo);
-        if( values !== undefined) {
-            let respuesta = calcularInteres(values);
-            result.innerText = respuesta ;
-        } else {
-            result.innerText ='Verifica los datos, solo debe de haber un "?" y números positivos.';
-        }
+    if (values !== undefined) {
+        let respuesta = calcularInteres(values);
+        result.innerText = respuesta;
+    } else {
+        result.innerText = 'Verifica los datos, solo debe de haber un "?" y números positivos.';
+    }
 }
 function calcularInteres(arr) {
-        let copy = [ ...arr ]; 
+    let copy = [...arr];
+    if (copy !== undefined) {
         //Deconstruct
-        if (copy !== undefined){
-            let [ i, c, r, t ] = copy;
-            if ( i === '?') {
-                return '$ ' + (+c * (+r/100) * +t);
-            } else if (c === '?') {
-                return '$ '+ ((+i)/(( +r/100 ) * +t));
-            } else if (r === '?'){
-                return (((+i)*100)/(+c * +t)) + ' %'
-            } else if (t === '?'){
-                return ((+i)/(+c * (+r/100))) + ' años'
-            }
-        } else {
-            return undefined ;
+        let [i, c, r, t] = copy;
+        if (i === '?') {
+            return '$ ' + (+c * (+r / 100) * +t);
+        } else if (c === '?') {
+            return '$ ' + ((+i) / ((+r / 100) * +t));
+        } else if (r === '?') {
+            return (((+i) * 100) / (+c * +t)) + ' %'
+        } else if (t === '?') {
+            return ((+i) / (+c * (+r / 100))) + ' años'
         }
+    } else {
+        return undefined;
+    }
 }
 
 function validateData(...theArg) {
     /* Esta función valida que solo haya un signo de interrogación
     y números positivos */
-    let copy = [ ...theArg ];
+    let copy = [...theArg];
     let questionMarksCount = count(copy, '?');
     let filterNumbers = copy.filter(item => item !== '?');
-    let areAllNumbers = filterNumbers.every(item => +item * 0 === 0 );
-    let areAllPositive = filterNumbers.every(item => +item >= 0 );
+    let areAllNumbers = filterNumbers.every(item => +item * 0 === 0);
+    let areAllPositive = filterNumbers.every(item => +item >= 0);
     // console.log(filterNumbers, areAllNumbers);
-    if(questionMarksCount === 1 && areAllNumbers && areAllPositive){
-        return copy ;
+    if (questionMarksCount === 1 && areAllNumbers && areAllPositive) {
+        return copy;
     } else {
-        return undefined ;
+        return undefined;
     }
-    
+
 }
 
 function count(arr, element) {
     // array,string/number -> number
     //busca el element en el array
     let copy = [...arr];
-    let found = copy.filter(item => item ===element);
+    let found = copy.filter(item => item === element);
     return found.length;
 }
